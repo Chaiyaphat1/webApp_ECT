4a2b965f6d7331cfc288da08b4721247440bc5df
chaiyaphat <chaiyaphat@hotmail.com>
2022-09-01 14:42:42 +0700

    work3

---
+++ post.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>post</title>
+</head>
+<body>
+<h1 style="text-align: center;">Webboard KakKak</h1>
+<div align="center">
+            ต้องการกูกระทู้หมายเลข  <?php echo $_GET["id"];?>
+        </div>
+        <hr>
+        <table style="border: 2px solid black;  margin-left: auto;margin-right: auto;">
+            <tr>
+                <td colspan="2" style="background-color: #6CD2FE;">แสดงความคิดเห็น</td>
+            </tr>
+            <td><textarea name="" width="400px" height="100px"> </textarea></td>
+            </tr>
+            <tr>
+                <td colspan="2" align="center"><input type="submit" value="ส่งข่อความ"></td>
+            </tr>
+        </table>
+        <br>
+        <div align="center">
+            <a href="index1.html">กลับไปหน้าหลัก</a>
+        </div> 
+</body>
+</html>
